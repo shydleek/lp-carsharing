bdafc78540ffe2e7458b20d2efa259f5b20ff5fb
Matthew <matiksh.2002@gmail.com>
2021-04-17 18:15:54 +0300

    Completed desktop-first landing page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 $(function(){
-
     $('.top__slider').slick({
         arrows: false,
         dots: true,
@@ -45,14 +44,4 @@ $(function(){
     $('.menu__btn').on('click', function(){
         $('.menu__list').toggleClass('menu__list--active');
     });
-
-    $('.reviews__slider').slick({
-        
-        arrows: false,
-        dots: true,
-        slidesToShow: 4,
-        slidesToScroll: 1
-
-    });
-
 });
